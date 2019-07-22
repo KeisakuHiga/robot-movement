f487ca751aab15841c254037571a8712855e1f48
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-07-22 22:39:29 +1000

    added readInputData function, and modified the related functions based on that change

--- src/app.js
+++ src/app.js
@@ -1,4 +1,2 @@
 const Robot = require('./script')
 const newRobot = new Robot()
-// newRobot.place(2,4,'n')
-// newRobot.run()
--- src/script.js
+++ src/script.js
@@ -1,28 +1,19 @@
-// const readlineSync = require('readline-sync');
 const fs = require('fs')
+
 class Robot {
   constructor() {
     this.x = 0
     this.y = 0
     this.f = 'NORTH'
     this.directions = ['NORTH', 'EAST', 'SOUTH', 'WEST']
-    this.readInput()
-  }
-  
-  // printCommands function might not necessary
-  printCommands = () => {
-    // console.log(`\nRobot is at [${this.x}, ${this.y}] and facing ${this.f}\n`)
-    // console.log('Choose your command!')
-    // const userChoice = readlineSync.questionInt('1. Place the robot to a new place!\n2. Move forward!\n3. Turn left!\n4. Turn right!\n5. Report the place where the robot is!\n6. Quit!\n');
-    return userChoice
+    this.commands = []
+    this.readInputData()
+    this.run(this.commands)
   }
 
 // read input data function from text file
-  readInput = () => {
-    fs.readFile('src/input/input1.txt', (err, data) => {
-      if(err) throw err;
-      console.log(data.toString())
-    })
+  readInputData = () => {
+    this.commands = fs.readFileSync('src/input/input1.txt').toString().split("\n")
   }
 // this will return an object that has x, y and f data, and the data will be
 // passed to place function which receive the object as an argument
@@ -33,26 +24,18 @@ class Robot {
 // if a PLACE command gives greater than 4 for x and y, and wrong directions
 // this command will return null
 
-  place = (inputObj) => {
-    // this.x = readlineSync.questionInt('Type a number from 0 ~ 4 for "X"\n')
-    while(this.x < 0 || this.x > 4 ) {
-      // console.clear()
-      // console.log('WARNING! Please type a valid number(0 ~ 4)\n')
-      // this.x = readlineSync.questionInt('Type a number from 0 ~ 4 for "X"\n')
-    }
-    console.clear()
-    // this.y = readlineSync.questionInt('Type a number from 0 ~ 4 for "Y"\n')
-    while(this.y < 0 || this.y > 4 ) {
-      // console.clear()
-      // console.log('WARNING! Please type a valid number(0 ~ 4)\n')
-      // this.y = readlineSync.questionInt('Type a number from 0 ~ 4 for "Y"\n')
-    }
-    console.clear()
-    // this.f = readlineSync.question('Type a direction from "NORTH", "SOUTH", "EAST" and "WEST"\n').toUpperCase() 
-    while(this.f !== "NORTH" && this.f !== "SOUTH" && this.f !== "EAST" && this.f !== "WEST") {
-      // console.clear()
-      // console.log('WARNING! Please type a valid direction!\n')
-      // this.f = readlineSync.question('Type a direction from "NORTH", "SOUTH", "EAST" and "WEST"\n').toUpperCase()
+  place = (line) => {
+    this.x = line.slice(6,7)
+    this.y = line.slice(8,9)
+    this.f = line.slice(10)
+    if(this.x < 0 || this.x > 4) {
+      return null
+    } else if(this.y < 0 || this.y > 4) {
+      return null
+    } else if (this.f !== "NORTH" && this.f !== "SOUTH" && this.f !== "EAST" && this.f !== "WEST") {
+      return null
+    } else {
+      return true
     }
   }
 
@@ -61,38 +44,30 @@ class Robot {
     switch(option) {
       case "NORTH":
         if(this.y === 4) {
-          // console.log('You cannot move to NORTH!')
         } else {
-          // console.log('Robot moved forward!\n')
           return this.y++
         }
         break;
       case "SOUTH":
         if(this.y === 0) {
-          // console.log('You cannot move to SOUTH!')
         } else {
-          // console.log('Robot moved forward!\n')
           return this.y--
         }
         break;
       case "EAST":
         if(this.x === 4) {
-          // console.log('You cannot move to EAST!')
         } else {
-          // console.log('Robot moved forward!\n')
           return this.x++
         }
         break;
       case "WEST":
         if(this.x === 0) {
-          // console.log('You cannot move to WEST!')
         } else {
-          // console.log('Robot moved forward!\n')
           return this.x--
         }
         break;
       default:
-        // console.log('Please enter a valid number!')
+        console.log('Please enter a valid number!')
     }
   }
 
@@ -117,46 +92,46 @@ class Robot {
   }
 
 // output data function which creates a text file to show the inputs data and 
-// the result even if
+// the result
   report = () => {
-    // console.log(`${this.x}, ${this.y}, ${this.f}`)
+    // fs.writeFile('src/output', data, (err) => {
+    //   if(err) throw err;
+    // })
+    console.log(`${this.x}, ${this.y}, ${this.f}`)
   }
 
-  // run function might not be necessary
-  run = () => {
-    let command
-    while( command !== 6) {
-      command = this.printCommands()
-      switch(command) {
-        case 1:
-          console.clear()
-          this.place()
-  
-          break;
-        case 2:
-          this.move()
-          break;
-        case 3:
-          this.left()
-          console.clear()
-          console.log('Robot turned left!\n')
-          break;
-        case 4:
-          this.right()
-          console.clear()
-          console.log('Robot turned right!\n')
-          break;
-        case 5:
-          console.clear()
-          console.log('Robot is at...\n')
-          this.report()
-          break;
-        case 6:
-          console.clear()
-          console.log('See you!')
-          break;
-        default:
-          console.log('Type a validate command number!\n')
+  run = (commands) => {
+    if(!commands[0].includes('PLACE')) {
+      return null
+    } else {
+      try {
+        commands.forEach(command => {
+          if(command.includes('PLACE')) {
+            const result = this.place(command)
+            if(result === null) {
+              throw('error')
+            }
+          } else {
+            switch(command) {
+              case 'MOVE':
+                this.move()
+                break;
+              case 'LEFT':
+                this.left()
+                break;
+              case 'RIGHT':
+                this.right()
+                break;
+              case 'REPORT':
+                this.report()
+                break;
+              default:
+                return null
+            }
+          }
+        })
+      } catch(err) {
+        console.log(err)
       }
     }
   }
